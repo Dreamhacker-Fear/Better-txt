fedf147d0654cbb546ac79e27f285333c2465969
Rᴏᴏᴛᴋɪᴛ™ <lucaebraeuer@gmail.com>
2026-08-27 02:37:38 +0200

    Refactor Settings component to include input fields

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -1,8 +1,28 @@
 import { Forms } from "@vendetta/ui/components";
-const { FormText } = Forms;
+import { storage } from "@vendetta/plugin";
 
-export default () => (
-    <FormText>
-        Hello, world!
-    </FormText>
-)
+const { FormSection, FormInput } = Forms;
+
+export default function Settings() {
+    return (
+        <FormSection title="Sentence Style">
+            <FormInput
+                title="Sentence Separator"
+                value={storage.separator ?? ", "}
+                placeholder=", "
+                onChange={(value: string) => {
+                    storage.separator = value;
+                }}
+            />
+
+            <FormInput
+                title="Sentence Ending"
+                value={storage.ending ?? "."}
+                placeholder="."
+                onChange={(value: string) => {
+                    storage.ending = value;
+                }}
+            />
+        </FormSection>
+    );
+}
